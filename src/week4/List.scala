3b72e8daf83ee57158270594e52bdc41deabe279
Abhijeet Jadhav <abhi.j24@gmail.com>
2016-03-30 10:27:47 +0530

    week4 code List.scala to understand polymorphism

--- src/week4/List.scala
+++ src/week4/List.scala
@@ -11,7 +11,42 @@ trait List[T] {
   
   // reference to remainder of list
   def tail: List[T]
+   
 }
 
 // implementation
-class cons[T](val)
+class Cons[T](val head: T, val tail: List[T]) extends List[T] {
+  def isEmpty = false
+}
+
+class Nil[T] extends List[T] { 
+  def isEmpty = true
+  def head: Nothing = throw new NoSuchElementException("Nil.head")
+  def tail: Nothing = throw new NoSuchElementException("Nil.tail")
+}
+
+object nth{
+  // function nth input int n and List l returns n'th element of lsit l
+  def nth[T](n: Int, xs: List[T]): T = {
+    if(xs.isEmpty) throw new IndexOutOfBoundsException
+    if(n==0) xs.head
+    else nth(n-1,xs.tail)
+  }
+  
+  //define list 1,2,3
+  val list = new Cons(1, new Cons(2, new Cons(3, new Nil)))
+  
+  def main(args: Array[String]) = 
+    println(nth(2,list))
+    //println(nth(-1,list))
+}
+
+
+
+
+
+
+
+
+
+
