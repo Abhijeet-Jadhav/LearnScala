a7fbb16ff3b9a4590cf97cb3fb410371c5e84c90
Abhijeet Jadhav <abhi.j24@gmail.com>
2016-04-07 08:58:53 +0530

    lists

--- src/Codes/P08.scala
+++ src/Codes/P08.scala
@@ -9,13 +9,23 @@ res0: List[Symbol] = List('a, 'b, 'c, 'a, 'd, 'e)
  */
 object P08 {
 
-  
+  def removeDupFunctional[A](ls: List[A]): List[A] = {
+    
+    val xs: List[A] = ls.foldLeft(List[A]())( (i,j) => {
+      if(i.last != j) 
+        j :: i
+      else 
+        i
+    })
+    
+    return xs
+  }
 
   def main(args: Array[String]) {
     val ls: List[Symbol] = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
     //println(removeDup(ls))
     //println(removeDupTailRec(ls))
-    //println(removeDupFunctional(ls))
+    println(removeDupFunctional(ls))
 
   }
 }
