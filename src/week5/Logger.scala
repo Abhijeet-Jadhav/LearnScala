cebedd85517621672427f3379a1f579992db8e77
Abhijeet Jadhav <abhi.j24@gmail.com>
2016-04-14 10:25:46 +0530

    Classes

--- src/week5/Logger.scala
+++ src/week5/Logger.scala
@@ -1,25 +1,28 @@
 package week5
 
 class Logger {
-  
+
   println("calling default constructor")
   var context: String = "generic"
-  
+
   def this(context: String) = { this(); this.context = context }
-  
+
   def info(m: String) = println(s"INFO: $context: $m")
   def error(m: String) = println(s"ERROR: $context: $m")
 }
 
-object Main1 extends App{
+/*
+class Logger(private var context: String = "generic") {
+  def info(m: String) = println(s"INFO: $context: $m")
+  def error(m: String) = println(s"ERROR: $context: $m")
+}
+//This way context cannot be accessed from outside the class
+*/
+object Main1 extends App {
 
-  
   val l = new Logger()
-
-  l.info("test message 1") // outputs `INFO: generic: test message 1`
-  
+  l.info("test message 1") // outputs `INFO: generic: test message 1`  
   l.context = "controller"
-  
   l.info("test message 2") // outputs `INFO: controller: test message 2`
 
 }
