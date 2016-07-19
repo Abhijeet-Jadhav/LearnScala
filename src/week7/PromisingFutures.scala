50c77b0f3a66482fdc348e4a6cfda3bfa3fc0f17
Abhijeet Jadhav <abhi.j24@gmail.com>
2016-07-19 23:55:16 +0530

    extract a value from the Future using callback

--- src/week7/PromisingFutures.scala
+++ src/week7/PromisingFutures.scala
@@ -1,16 +1,34 @@
 package week7
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.util.{Success, Failure}
 
-object PromisingFutures  {
+
+class PromisingFutures{
   
   val oneFuture: Future[Int] = Future {
     Thread.sleep(1000)
     1
   }
   
+  val oneDangerousFuture=Future{
+    Thread.sleep(2000)
+    throw new SomeComputationException("Welcome to the Dark side !")
+  }
+
+  case class SomeComputationException(msg: String) extends Exception(msg)
   
-  def checkState(): Unit = {  
+  def printFuture[T](future: Future[T]): Unit = future.onComplete {  
+    case Success(result) => println(s"Success $result")
+    case Failure(throwable) => println(s"Failure $throwable")
+  }
+  
+}
+object PromisingFutures  {
+  
+  
+  
+  /*def checkState(): Unit = {  
     
     // isCompleted : Returns whether the future has already been completed with a value or an exception.
     
@@ -22,9 +40,16 @@ object PromisingFutures  {
     Thread.sleep(1100)
     println(s"Completed : ${oneFuture.isCompleted}, Value : ${oneFuture.value}")
 
-  }
+  }*/
+  
   
   def main(args: Array[String]){
-    checkState()
+    //checkState()
+    
+    val promisingFutures=new PromisingFutures
+    promisingFutures.printFuture(promisingFutures.oneFuture)
+    promisingFutures.printFuture(promisingFutures.oneDangerousFuture)
+
+    synchronized(wait(3000))
   }
 }
